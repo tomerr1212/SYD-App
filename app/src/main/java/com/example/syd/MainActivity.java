2a3ed00eea3b67d80fbaa73f5384fba921613001
tomerr1212 <tomermaabari@gmail.com>
2019-11-14 22:11:24 +0200

    updates

--- app/src/main/java/com/example/syd/MainActivity.java
+++ app/src/main/java/com/example/syd/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.syd;
 
+import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signup = (Button) findViewById(R.id.SignUp_button);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignUp();
+            }
+        });
     }
+
+    public void openSignUp(){
+        Intent intent = new Intent(this, signup.class);
+        startActivity(intent);
+
+    }
+
 }
