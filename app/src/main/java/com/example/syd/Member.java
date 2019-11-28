5dca8595a2c83f46e0acf6da749954f119c1e498
tomer <tomerr1212@gmail.com>
2019-11-28 11:07:25 +0200

    member

---
+++ app/src/main/java/com/example/syd/Member.java
@@ -0,0 +1,28 @@
+package com.example.syd;
+
+public class Member {
+
+    private String Name;
+    private int Age;
+
+
+    public Member(){
+
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+}
