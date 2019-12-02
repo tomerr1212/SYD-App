96ad98506af1ae2f0b7a84c61142888c791c6e3c
orti <orti@gmail.com>
2019-12-02 13:10:10 +0200

    Facebook button login | Ortal

--- app/src/main/java/com/example/syd/MainActivity.java
+++ app/src/main/java/com/example/syd/MainActivity.java
@@ -112,7 +112,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             updateUI();
                         }
 
-                        // ...
+                        // .
                     }
                 });
     }
