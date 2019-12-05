b4c999cdaade98c1f29b4a55aa62de557610cc4c
tomer <tomerr1212@gmail.com>
2019-12-05 12:46:24 +0200

    member

---
+++ app/src/main/java/com/example/syd/Member.java
@@ -0,0 +1,52 @@
+package com.example.syd;
+
+public class Member {
+
+    private String Name,Gender;
+    private int Age,Weight,Height;
+
+
+    public Member(){
+
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public int getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(int weight) {
+        Weight = weight;
+    }
+
+    public int getHeight() {
+        return Height;
+    }
+
+    public void setHeight(int height) {
+        Height = height;
+    }
+}
