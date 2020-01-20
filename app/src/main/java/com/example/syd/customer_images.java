01984cdae1e7b0e4fa25794b226ffbceb7281252
tomer <tomerr1212@gmail.com>
2020-01-20 14:20:50 +0200

    working on retrieving image

--- app/src/main/java/com/example/syd/customer_images.java
+++ app/src/main/java/com/example/syd/customer_images.java
@@ -1,13 +1,16 @@
 package com.example.syd;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
 import com.google.firebase.storage.StorageReference;
 import com.toptoche.searchablespinnerlibrary.SearchableSpinner;
 
@@ -16,7 +19,7 @@ public class customer_images extends AppCompatActivity {
     ImageView imageViewCustomerImage;
     FirebaseAuth mAuth;
     FirebaseStorage storageAuth;
-
+    Image image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +28,32 @@ public class customer_images extends AppCompatActivity {
         searchableSpinnerViewImages = findViewById(R.id.searchableViewImages);
         imageViewCustomerImage = findViewById(R.id.imageViewCustomerImage);
         storageAuth = FirebaseStorage.getInstance();
+
         mAuth = FirebaseAuth.getInstance();
+
+        Log.v("WOW", storageAuth.getReference().child("ImageFolder").child(mAuth.getCurrentUser().getUid()).toString());
+
+        storageAuth.getReference().child("ImageFolder").child(mAuth.getCurrentUser().getUid()).listAll()
+                .addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                                          @Override
+                                          public void onSuccess(ListResult listResult) {
+                                              for (StorageReference item : listResult.getItems()) {
+                                                  Log.v("WOW", item.getName());
+
+                                                  String url = "gs://sydfirebaseproject-60fc3.appspot.com/ImageFolder/Tz7dsF6cxjZap2bPWB5RvoImIQj2/imageimage:34" + item.getName() + "?alt=media";
+
+                                                  Log.w("WOW", url);
+                                              }
+                                          }
+                                      });
+
+        Log.v("CHECKING",storageAuth.getReference().child("ImageFolder").child(mAuth.getCurrentUser().getUid()).toString());
         StorageReference imageReff = storageAuth.getReference().child("ImageFolder").child(mAuth.getCurrentUser().getUid());
-        Glide.with(customer_images.this /* context */)
-                .load(imageReff)
-                .into(imageViewCustomerImage);
-
-
+//        Glide.with(customer_images.this /* context */)
+//                .load(imageReff)
+//                .into(imageViewCustomerImage);
+//
+//
 
 
 
